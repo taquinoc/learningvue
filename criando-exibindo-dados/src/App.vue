ffac28d3acbb4b613b5bec4ac93fa31ca4bead31
thiagoac33 <aquino.thiago33@gmail.com>
2019-10-16 12:18:57 -0300

    Exercicios

--- criando-exibindo-dados/src/App.vue
+++ criando-exibindo-dados/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <h1 v-once v-text="titulo"></h1>
-    <div v-if="tarefas.length <= 0"> 
-      Não há tarefas
-    </div>
-    <div v-if ="tarefas.length">
-      Existem {{tarefas.length}} tarefas
-    </div>
+    <div v-if="tarefas.length <= 0">Não há tarefas</div>
+    <div v-if="tarefas.length">Existem {{tarefas.length}} tarefas</div>
 
     <div v-if="tarefas.length > 0">
       <ul>
-        <li v-for="tarefa in tarefas"> {{tarefa}}</li>
+        <li v-for="tarefa in tarefas">{{tarefa}}</li>
       </ul>
     </div>
+    <button v-on:click="tarefas = []">Limpar</button>
+
+    <input v-bind:disabled="tarefas.length == 0" v-model = "subtitulo" type="text" name="" id="">
     
     <h2>{{subtitulo}}</h2>
   </div>
@@ -20,20 +19,27 @@
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      titulo: 'Listagem de tarefas',
-      subtitulo: 'Defina uma descrição',
-      tarefas: ['Melhorar no HTML','Melhorar no JS', 'Voar com o VueJS']
-    }
+      titulo: "Listagem de tarefas",
+      subtitulo: "Defina uma descrição",
+      tarefas: ["Melhorar no HTML", "Melhorar no JS", "Voar com o VueJS"]
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.titulo = "novo titulo";
+      this.tarefas = ["Tarefa 1", "Tarefa 2", "Tarefa 3", "Nova tarefa 1", "Nova tarefa 2", "Nova tarefa 3"];
+    }, 3000);
   }
-}
+};
+
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -41,7 +47,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
---
+++ exercises/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1 align="center">Retorno de metadados com {{titulo}}</h1>
+    <small>
+      {{nome}}
+    </small>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        titulo: 'VueJS!',
+        nome: 'Mané Garrincha',
+      }
+    },
+    mounted(){
+      setTimeout(()=> this.nome = "Mineirão", 4000)
+    }
+  }
+</script>
+
+<style>
+
+
+</style>
